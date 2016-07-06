a3060eea99f68fe636e91c70201007248ab88758
Maciej Woźniak <iskeld@gmail.com>
2016-07-06 22:44:28 +0200

    Extract db file name to attribute

--- lib/elkarmo/store.ex
+++ lib/elkarmo/store.ex
@@ -1,6 +1,8 @@
 defmodule Elkarmo.Store do
   use GenServer
 
+  @db_file :karma_db
+
   def start_link(karma), do: GenServer.start_link(__MODULE__, karma, name: __MODULE__)
 
   def get, do: GenServer.call __MODULE__, :get
@@ -8,7 +10,7 @@ defmodule Elkarmo.Store do
   def set(new_karma), do: GenServer.cast __MODULE__, {:set, new_karma}
 
   def init(initial_karma) do
-    {:ok, table} = :dets.open_file(:karma_db, [type: :set])
+    {:ok, table} = :dets.open_file(@db_file, [type: :set])
     karma = case :dets.lookup(table, :karma) do
       [karma: found_karma] -> found_karma
       [] -> initial_karma
@@ -20,12 +22,12 @@ defmodule Elkarmo.Store do
   def handle_call(:get, _from, state), do: {:reply, state, state}
 
   def handle_cast({:set, new_karma}, _current_karma) do
-    :dets.insert(:karma, {:karma, new_karma})
+    :dets.insert(@db_file, {:karma, new_karma})
     :dets.sync(:karma)
     {:noreply, new_karma}
   end
 
   def terminate(_reason, _state) do
-    :dets.close(:karma_db)
+    :dets.close(@db_file)
   end
 end
