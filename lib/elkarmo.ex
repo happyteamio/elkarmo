432750aae0dd0b3089d8f0ff4590cab754e792e7
Maciej Woźniak <iskeld@gmail.com>
2016-07-07 21:18:55 +0200

    Move slack token to config.exs

--- lib/elkarmo.ex
+++ lib/elkarmo.ex
@@ -4,9 +4,11 @@ defmodule Elkarmo do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    slack_token = Application.get_env(:elkarmo, :slack_token)
+
     children = [
       worker(Elkarmo.Store, [Elkarmo.Karma.empty]),
-      worker(Elkarmo.Slack, [""])
+      worker(Elkarmo.Slack, [slack_token])
     ]
     opts = [strategy: :one_for_one, name: Elkarmo.Supervisor]
     {:ok, _pid} = Supervisor.start_link children, opts
